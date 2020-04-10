0a15bdfc98bdddbc276b6f193035a7f22a0146ff
jeff <jluicanada@gmail.com>
2020-04-10 12:19:24 +0900

    Generated models and database for categories. Added time stamp for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :summary, presence: true
+    validates :body, presence: true
+
+    def details
+        "This post was created on #{ created_at.stftime("%d %M %Y") }"
+    end
+   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_043010) do
+ActiveRecord::Schema.define(version: 2020_04_10_025404) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
